a086b2da996c58ea433b223813e68dd2ed14e011
alih6051 <alih6051@gmail.com>
2022-12-17 17:32:34 +0530

    initial

--- src/Components/ProductsPage/ProductItem.jsx
+++ src/Components/ProductsPage/ProductItem.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { GridItem, Image, Box, Text, Button } from "@chakra-ui/react";
 import ReactStars from "react-rating-stars-component";
+import { Link as RouterLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { formatCurrency } from "../../utils/formatCurrency";
 
-const ProductItem = ({ category, image, price, rating, title }) => {
+const ProductItem = ({ image, price, rating, title, id }) => {
+  let { pathname } = useLocation();
+
   return (
     <GridItem border="1px solid #dadcdf">
-      <Image src={image} alt={title} />
-      <Box padding="7px" h="130px">
-        <Text fontSize="lg" color="#c7202c">
-          Sale INR {price}
-        </Text>
-        <ReactStars size={25} value={rating} edit={false} isHalf={true} />
-        <Text fontSize="sm" noOfLines={2}>
-          {title}
-        </Text>
-      </Box>
+      <RouterLink to={`${pathname}/${id}`}>
+        <Image src={image} alt={title} />
+        <Box padding="7px" h="130px">
+          <Text fontSize="lg" color="#c7202c">
+            Starting at {formatCurrency(price)}
+          </Text>
+          <ReactStars size={25} value={rating} edit={false} isHalf={true} />
+          <Text fontSize="sm" noOfLines={2}>
+            {title}
+          </Text>
+        </Box>
+      </RouterLink>
       <Button borderRadius="0" width="100%">
         Add to Cart
       </Button>
---
+++ src/Components/SingleProductPage/ProductImage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box, Image } from "@chakra-ui/react";
+
+const ProductImage = ({ image }) => {
+  return (
+    <Box bg="tomato">
+      <Image
+        src={image}
+        width="100%"
+        alt="Dan Abramov"
+        border="1px solid #dadcdf"
+      />
+    </Box>
+  );
+};
+
+export default ProductImage;
